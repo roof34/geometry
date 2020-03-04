084dca6137f584150350bfb5cea4de149b003174
roof34 <github.com/roof34>
2020-03-05 06:51:30 +0700

    Create point.h

---
+++ point.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+typedef struct point {
+    int x;
+    int y;
+} point_t;
+
+typedef struct circle {
+    point_t O;
+    float r;
+} circle;
+
+typedef struct triangle {
+    point_t P[4];
+} triangle;
+
+void inputcirlce(circle* c);
+void outputcirlce(circle* c, FILE* f);
+void inputtriangle(triangle* t);
+void outputtriangle(triangle* t, FILE* f);
