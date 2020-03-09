0ab2696c3c69a6f77d1ba6507ed84f30206b66d8
roof34 <github.com/roof34>
2020-03-09 22:19:16 +0700

    Create main

--- main.c
+++ main.c
@@ -1,21 +1,29 @@
 #include "point.h"
 #include <stdio.h>
-
+#include <string.h>
 int main()
 {
     FILE* f = fopen("file", "w");
-    circle c;
-    
-    intputcirlce(&c);
-
-    triangle t;
-
-    inputtriangle(&t);
-
-    outputcirlce(&c, f);
-
-    outputtriangle(&t, f);
-
+    int n;
+    char tr[10]="triangle";
+    char cr[10]="circle";
+    char fg[15];
+    printf("Input amount of figures: ");
+    scanf("%d",&n);
+    for (int i=0;i<n;i++){
+	scanf("%c",fg);
+	if ( strcmp(cr,fg)==0){
+	    circle c;
+	    inputcirlce(&c);
+	     outputcirlce(&c, f);
+}
+	else if ( strcmp (tr,fg) ==0){
+	    triangle t;
+	    inputtriangle(&t);
+	    outputtriangle(&t, f);
+}
+	else printf("unknown shape  ");
+}
     fclose(f);
 
     return 0;
