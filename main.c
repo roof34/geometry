d0565aeec5232ffed8f17bda6c1ce29f3bc7abfa
roof34 <https://github.com/roof34/geometry>
2020-03-10 09:27:35 +0700

    Change main.c

--- main.c
+++ main.c
@@ -5,26 +5,25 @@ int main()
 {
     FILE* f = fopen("file", "w");
     int n;
-    char tr[10]="triangle";
-    char cr[10]="circle";
+    char tr[10] = "triangle";
+    char cr[10] = "circle";
     char fg[15];
     printf("Input amount of figures: ");
-    scanf("%d",&n);
-    for (int i=0;i<n;i++){
-	printf("Enter figure ");
-	scanf("%s",fg);
-	if ( strcmp(cr,fg)==0){
-	    circle c;
-	    inputcirlce(&c);
-	     outputcirlce(&c, f);
-}
-	else if ( strcmp (tr,fg) ==0){
-	    triangle t;
-	    inputtriangle(&t);
-	    outputtriangle(&t, f);
-}
-	else printf("unknown shape  ");
-}
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("Enter figure ");
+        scanf("%s", fg);
+        if (strcmp(cr, fg) == 0) {
+            circle c;
+            inputcirlce(&c);
+            outputcirlce(&c, f);
+        } else if (strcmp(tr, fg) == 0) {
+            triangle t;
+            inputtriangle(&t);
+            outputtriangle(&t, f);
+        } else
+            printf("unknown shape  ");
+    }
     fclose(f);
 
     return 0;
